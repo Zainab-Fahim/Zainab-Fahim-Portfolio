c954137ba4b6ba5e74d42b84566c827c0a368eef
Zainab-Fahim <shafna.zainab.fahim@gmail.com>
2023-03-01 18:43:26 +0530

    initial commit

---
+++ src/data/socialsData.js
@@ -0,0 +1,12 @@
+export const socialsData = {
+    github: 'https://github.com/Zainab-Fahim',
+    linkedIn: 'https://www.linkedin.com/in/zainab-fahim/',
+    codepen: 'https://codepen.io/',
+    twitter: 'https://twitter.com/ZainabFahim9',
+    reddit: 'https://www.reddit.com/user/',
+    blogger: 'https://www.blogger.com/',
+    medium: 'https://medium.com/@',
+    stackOverflow: 'https://stackoverflow.com/users/',
+    gitlab: 'https://gitlab.com/',
+    youtube: 'https://www.youtube.com/@zainab-fahim722'
+}
